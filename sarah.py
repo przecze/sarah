679604d9d79862391a48a59bf66aae9db5a30ba5
Przemyslaw Czechowski <przemyslaw.czechowski@gmail.com>
2023-03-03 23:11:52 +0100

    Fixes

--- sarah.py
+++ sarah.py
@@ -4,7 +4,12 @@ import IPython
 import openai
 
 PROMPT = """
-You are a coding assistant integrated into interactive ipython shell. You take requests from the user in natural language and produce code snippets that the user can execute right away in their ipython session. Your only output are code snippets. Make sure all code snippets are surrounded with '```'. You may output multiple code snippets. Make sure every code snippet make sense in context of previously generated ones (for example, it reuses previously defined variables)
+You are a coding assistant integrated into interactive ipython shell. You take
+requests from the user in natural language and produce code snippets that the
+user can execute right away in their ipython session. Your only output are code
+snippets. Make sure all code snippets are surrounded with '```'. You may output
+multiple code snippets. Make sure every code snippet make sense in context of
+previously generated ones (for example, it reuses previously defined variables)
 """
 class MetaAsk(type):
     @property
@@ -27,16 +32,25 @@ class ask(object, metaclass=MetaAsk):
         session = cls.get_current_session()
         text = input('> ')
         session.append({"role": "user", "content": text})
-        completion = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo", 
-                messages=session
-            )
+        for trial in range(3):
+            completion = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo", 
+                    messages=session
+                )
 
-        message = completion['choices'][0]['message']
-        session.append(message)
-        cls.current_session = session
-        snippets = message['content'].split('```')[1::2]
-        snippets = dict(enumerate(snippets, start=1))
+            message = completion['choices'][0]['message']
+            session.append(message)
+            cls.current_session = session
+            snippets = message['content'].split('```')[1::2]
+            snippets = [s.strip() for s in snippets]
+            snippets = [s[len('python'):] if s.startswith('python') else s for s in snippets]
+            snippets = [s.strip() for s in snippets]
+            snippets = dict(enumerate(snippets, start=1))
+            if snippets:
+                break
+        else:
+            raise RuntimeError('no code snippets after 3 tries')
+
         for i, s in snippets.items():
             print(f"[{i}]: {s}")
 
